5ab35a648a8819a110ab03a63ae7a2b7cd19e673
Hanif M. <86345153+hanifmasy@users.noreply.github.com>
2025-06-13 15:01:33 +0700

    Update api.ts - change url api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 export async function fetchUsers() {
-  const res = await fetch("http://localhost:3000/users"); // FULL URL
+  const res = await fetch("http://localhost:4020/users"); // FULL URL
   if (!res.ok) throw new Error("Failed to fetch users");
   return res.json();
 }
